568fe64aea278bccba73eedced8cdd8fdfe1f9c4
B0ND07 <mhashimkp@gmail.com>
2024-03-13 19:41:47 +0530

    client: implement responsive navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen bg-rose-400">
+    <SiteNavigation/>
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import SiteNavigation from "./components/NavigationBar.vue"
 </script>
 
 <style lang="scss" scoped></style>
